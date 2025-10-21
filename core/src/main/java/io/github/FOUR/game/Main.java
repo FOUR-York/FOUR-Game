9d6f5069a51c4fbeb0fc5abcd1ba17e5e78da26d
Dipling <oscar.macdonald@outlook.com>
2025-10-21 16:09:28 +0100

    a bit of setup

--- core/src/main/java/io/github/FOUR/game/Main.java
+++ core/src/main/java/io/github/FOUR/game/Main.java
@@ -1,32 +1,39 @@
 package io.github.FOUR.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+    public static int WORLD_WIDTH = 640, WORLD_HEIGHT = 360;
+
+    private OrthographicCamera camera;
+    private FitViewport viewport;
     private SpriteBatch batch;
-    private Texture image;
 
     @Override
     public void create() {
+        camera = new  OrthographicCamera();
+        viewport =  new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        ScreenUtils.clear(0f, 0f, 0f, 1f);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 }
